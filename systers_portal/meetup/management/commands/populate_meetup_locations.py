af551787f130d2f9e9b620d4ce945d7152901619
sa7i <poojithansl007@gmail.com>
2017-10-15 15:46:23 +0530

    PEP8 Happy

--- systers_portal/meetup/management/commands/populate_meetup_locations.py
+++ systers_portal/meetup/management/commands/populate_meetup_locations.py
@@ -1,22 +1,14 @@
 from django.core.management.base import BaseCommand
-
-from django.contrib.auth.models import User
-from django.test import TestCase
-from django.utils import timezone
-from cities_light.models import City, Country
-
-from django.utils.crypto import get_random_string
-from meetup.models import MeetupLocation, Meetup, Rsvp
-from users.models import SystersUser
-import random
-import time
-import datetime 
-from pprint import pprint
+from cities_light.models import City
+from meetup.models import MeetupLocation, Meetup
 from slugify import slugify
 from optparse import make_option
+import random
+import time
+import datetime
+
 
 def strTimeProp(start, end, format, prop):
-
     """Get a time at a proportion of a range of two formatted times.
 
     start and end should be strings specifying times formated in the
@@ -35,27 +27,25 @@ def strTimeProp(start, end, format, prop):
 
 def randomDate(start, end, prop):
     format_str = '%Y-%m-%d %H:%M:%S'
-    #t =  strTimeProp(start, end, '%m/%d/%Y %I:%M %p', prop)
-    t =  strTimeProp(start, end, format_str, prop)
+    t = strTimeProp(start, end, format_str, prop)
     return datetime.datetime.strptime(t, format_str)
-
 
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
-        make_option('--locations', type=int, 
-                help="Number of meetup locations to seed with"),
-        make_option('--meetups', type=int, 
-                help="Number of meetups to seed with"))
-
+        make_option('--locations', type=int,
+                    help="Number of meetup locations to seed with"),
+        make_option('--meetups', type=int,
+                    help="Number of meetups to seed with"))
 
     def _get_fancy_title_and_slug(self, city):
         def _aux():
             period = ["annual", "weekly", "monthly"]
-            meeting_type = ["release party", "hackathon", "conference", "workshop", "meeting"]
+            meeting_type = ["release party", "hackathon",
+                            "conference", "workshop", "meeting"]
             second = random.choice(period)
             third = random.choice(meeting_type)
-            return "%s %s %s"%(city.name, second, third)
+            return "%s %s %s" % (city.name, second, third)
 
         while True:
             value = _aux()
@@ -77,45 +67,42 @@ class Command(BaseCommand):
                     break
             print(city)
             kwargs = {
-                #"city" : city,
-                "name" : city.name,
-                "slug" : slugify(city.name),
-                "location" : city,
-                "description" : "Bazinga!",
-                #"organizers" : get_random_string(length=4),
+                "name": city.name,
+                "slug": slugify(city.name),
+                "location": city,
+                "description": "Bazinga!",
+                # "organizers" : get_random_string(length=4),
             }
             meetup_table = MeetupLocation.objects.create(**kwargs)
-            meetup_table.save() 
+            meetup_table.save()
 
     def _create_meetups(self, **kw):
         Meetup.objects.all().delete()
         meetup_locations = MeetupLocation.objects.all()
         self.used_name = set()
         for x in range(kw['count']):
-            datetime_obj =  randomDate("2008-12-11 00:00:00", 
-                    "2017-12-11 00:00:00", random.random())
+            datetime_obj = randomDate("2008-12-11 00:00:00",
+                                      "2017-12-11 00:00:00", random.random())
 
             # Possible bug - duct tape workaround
             city = None
-            title, slug = '-'*51, '-'*51
+            title, slug = '-' * 51, '-' * 51
             while len(title) > 50 and len(slug) > 50:
                 city = random.choice(meetup_locations)
-                title, slug = self._get_fancy_title_and_slug(city.location) 
+                title, slug = self._get_fancy_title_and_slug(city.location)
             print(title, slug)
             kwargs = {
-                "title" : title,
-                "slug" : slug,
-                "description" : "saturday",
-                "meetup_location" : city,
-                "date" : datetime_obj.date(),
-                "time" : datetime_obj.time(),
+                "title": title,
+                "slug": slug,
+                "description": "saturday",
+                "meetup_location": city,
+                "date": datetime_obj.date(),
+                "time": datetime_obj.time(),
             }
 
             meetup = Meetup.objects.create(**kwargs)
             meetup.save()
-    
 
     def handle(self, *args, **options):
         self._create_meetup_locations(count=options['locations'])
         self._create_meetups(count=options['meetups'])
-
